d95f32fbf52f02c0c44f33e869f71c202263cce7
Andrey <andrey@gmail.com>
2026-05-09 11:45:46 +0300

    Prepare app for Render deploy

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
     console.log("Пользователь подключился");
